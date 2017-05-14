72274dfca597dd2ef6eab056d682fb5a457cd756
Aleistar Markoczy <a.markoczy@gmail.com>
2017-05-14 19:37:02 +0200

    MainView: Layout to code, Buttons horizontal

--- src/main/java/ch/bfh/bti7081/s2017/red/mhc_pms/ui/prefabs/IconButton.java
+++ src/main/java/ch/bfh/bti7081/s2017/red/mhc_pms/ui/prefabs/IconButton.java
@@ -5,29 +5,38 @@
  */
 package ch.bfh.bti7081.s2017.red.mhc_pms.ui.prefabs;
 
+import com.vaadin.event.MouseEvents.ClickListener;
 import com.vaadin.server.ThemeResource;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickListener;
-import com.vaadin.ui.themes.ValoTheme;
+import com.vaadin.ui.Image;
 
-public class IconButton extends Button
+public class IconButton extends Image
 {
+	// http://stackoverflow.com/questions/40847123/vaadin-using-an-image-as-a-button
+	
 	private static final long serialVersionUID = 1L;
 
 	private static final String BUTTON_RESOURCE = "buttons/";
 
 	public IconButton(String aRelativeName, ClickListener aListener)
 	{
-		super();
-//		setStyleName(ValoTheme.BUTTON_LINK);
-		setIcon(new ThemeResource(BUTTON_RESOURCE + aRelativeName));
+		super(null, new ThemeResource(BUTTON_RESOURCE + aRelativeName)); // TODO class resource!
+		
+		setPrimaryStyleName("iconbutton");
+		//		setStyleName(ValoTheme.BUTTON_LINK);
+//		setIcon(new ThemeResource(BUTTON_RESOURCE + aRelativeName));
 
-		addStyleName(ValoTheme.BUTTON_ICON_ONLY);
-		addStyleName(ValoTheme.BUTTON_BORDERLESS);
+//		addStyleName(ValoTheme.B);
+//		addStyleName(ValoTheme.BUTTON_BORDERLESS);
+//		addStyleName(ValoTheme.BUTTON_ICON_ALIGN_TOP);
+//		addStyleName(ValoTheme.BUTTON_ICON_ALIGN_RIGHT);
+//		addStyleName(ValoTheme.BUTTON_ICON_ONLY);
 		
 ////		setSizeUndefined();
-		setHeight("40px");
-		setWidth("148px");
+//		setHeight("38px");
+//		setWidth("146px");
+//		addClickListener(aListener);
+//		addStyleName("pointer");
+		
 		if (aListener != null) addClickListener(aListener);
 	}
 
